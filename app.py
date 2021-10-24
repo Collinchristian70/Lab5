a389589d65c7121847f6c0994d5a89a20b5b77a5
konnorscott22 <konnorscott@tamu.edu>
2021-10-24 16:37:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,39 +2,46 @@ from flask import Flask, request, render_template
 import hashlib
 from typing import Sequence
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+  return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0")
 
+
+
 #MD5 Code Start
-print("Input MD5 string for conversion")
-str = input()
-encoded_str = str.encode()
-hash_obj = hashlib.md5(encoded_str)
-hexa_value = hash_obj.hexdigest()
-print("\n", hexa_value, "\n")
+def MD5():
+    print("Input MD5 string for conversion")
+    str = input()
+    encoded_str = str.encode()
+    hash_obj = hashlib.md5(encoded_str)
+    hexa_value = hash_obj.hexdigest()
+    print("\n", hexa_value, "\n")
 #MD5 Code End
+MD5()
 
 #Factorial Code Start
-print("Input an integer")
-num = int(input())
-factorial = 1
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
-print("")
+def Factorial(): 
+    print("Input an integer")
+    num = int(input())
+    factorial = 1
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    elif num == 0:
+        print("The factorial of 0 is 1")
+    else:
+        for i in range(1,num + 1):
+            factorial = factorial*i
+    print("The factorial of",num,"is",factorial)
+    print("")
 #Factorial Code End
+Factorial()
 
 # Fibonacci Code Start
 def fibonacci (n):
@@ -47,27 +54,26 @@ def fibonacci (n):
         sequence.append(next_num)
     return sequence
 
-sequence = fibonacci(10)
-print(sequence)
+    sequence = fibonacci(10)
+    print(sequence)
 # Fibonacci Code End
 
 # Prime Code Start
+def Prime():
+    n = int(input("Enter a number: "))
 
-n = int(input("Enter a number: "))
+    if n > 1:
+        for i in range(2, n//2):
+            if(n % i) == 0:
+                print(n,": False")
+                break
+        else:
+            print(n,": True")
 
-if n > 1:
-    for i in range(2, n//2):
-        if(n % i) == 0:
-            print(n,": False")
-            break
-    else:
-        print(n,": True")
-
-elif type(n) != int:
-    print("Invalid")
-
+    elif type(n) != int:
+        print("Invalid")
 # Prime Code End
-
+Prime()
 
 # Slack Message Start
 import sys
@@ -101,7 +107,7 @@ def main(argv):
 
     send_slack_message(message)
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+    if __name__ == "__main__":
+        main(sys.argv[1:])
     
 # Slack Message End

4d1ed026160b58bd0270385fa6c651a51be76ba8
Gustavo Garcia-Taunton <62920572+gustavogarciataunton@users.noreply.github.com>
2021-10-24 10:45:23 -0500

    Update is-prime.py

--- is-prime.py
+++ is-prime.py
@@ -1,11 +1,12 @@
 n = int(input("Enter a number: "))
 
-if n>1:
-    for i in range(n, n//2):
-        if(n%i)==0:
-            print(n, ": False")
+if n > 1:
+    for i in range(2, n//2):
+        if(n % i) == 0:
+            print(n,": False")
             break
     else:
-        print(n, ": True")
-else:
-    print(n, "Invalid")
+        print(n,": True")
+
+elif type(n) != int:
+    print("Invalid")

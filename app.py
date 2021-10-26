8cbdcb6daf0d9a3ac7d637af0c9e8f5949742415
konnorscott22 <konnorscott@tamu.edu>
2021-10-26 16:45:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,53 @@
-# from flask import Flask, request, render_template
+from flask import Flask, json, jsonify, request, Response
+import os
 import hashlib
-from typing import Sequence
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-# if __name__ == '__main__':
-#     app.debug = True
-#     app.run(host="0.0.0.0")
+@app.route("/")
+def hello_world():
+     return jsonify(input='Howdy!', output= 'Hello World')
 
 #MD5 Code Start
-print("Input MD5 string for conversion")
-str = input()
-encoded_str = str.encode()
-hash_obj = hashlib.md5(encoded_str)
-hexa_value = hash_obj.hexdigest()
-print("\n", hexa_value, "\n")
+@app.route('/md5/<string:str>')
+def MD5(str):
+    encoded_str = str.encode()
+    hash_obj = hashlib.md5(encoded_str)
+    hexa_value = hash_obj.hexdigest()
+    return jsonify(input=str, output=hash_obj.hexdigest())
 #MD5 Code End
 
 #Factorial Code Start
-print("Input an integer for a factorial conversion")
-num = int(input())
-factorial = 1
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
-print("")
+@app.route('/factorial/<int:num>')
+def Factorial(num):
+    num = int(input())
+    factorial = 1
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    elif num == 0:
+        print("The factorial of 0 is 1")
+    else:
+        for i in range(1,num + 1):
+           factorial = factorial*i
+
+    return jsonify(input=num, output=factorial)
 #Factorial Code End
 
 # Fibonacci Code Start
-print("Input an integer for the Fibonacci sequence")
-n = int(input())
-def fibonacci (n):
-    if n < 0:
-        print("Incorrect input")
-    sequence = [0,1]
-    for i in range(2,n+1):
-        next_num = sequence[-1] + sequence[-2]
+@app.route("/fibonacci/<int:number>")
+def calc_fibonacci(n):
+    n = int(input())
+    def fibonacci (n):
+        if n < 0:
+            return jsonify(input=n, output="Error: Please enter a number greater or equal to 0")
+        sequence = [0,1]
+        for i in range(2,n+1):
+            next_num = sequence[-1] + sequence[-2]
 
-        sequence.append(next_num)
-    return sequence
-
-sequence = fibonacci(n)
-print(sequence)
+            sequence.append(next_num)
+        return jsonify(input=n, output=sequence)
 # Fibonacci Code End
 
-print("")
 
 # Prime Code Start
 print("Enter a number to check if its prime: ")
@@ -72,40 +66,28 @@ def fibonacci(n):
 print(fibonacci(n))
 # Prime Code End
 
-
-# Slack Message Start
-import sys
-import getopt 
-
-def send_slack_message(message):
-    payload = '{"text":"s%"}' % message
-    response = requests.post('https://hooks.slack.com/services/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',
-     data=payload)
-
-    print(response.text)
-
-def main(argv):
-
-    message = ' '
-
-    try: opts, args = getopt.getopt(argv, "hm:", ["message="])
-
-    except getopt.GetoptError:
-        print('Slackmessage.py -m <message>')
-        sys.exit(2)
-    if len(opts) == 0:
-        messege = "No, input detected" 
-    for opt, arg in opts:
-        if opt == '-h':
-            print('Slackmessage.py -m <message>')
-            sys.exit()
-        elif opt in ("-m", "--message"):
-            message = arg 
+@app.route('/slack-alert/<string:msg>')
+def slack(msg):
+	try:
+		response = client.chat_postMessage(
+			channel="C011KJWHA22",
+			text=msg
+		)
+		print("RESPONSE:", response)
+		return jsonify(
+			input=msg,
+			message=msg,
+			output= "Message was successfully sent in slack" if response['ok'] else ''
+		), 200 if response['ok'] else 400
+	except SlackApiError as e:
+		assert e.response["error"]
+		return jsonify(
+			input=msg,
+			message=msg,
+			output=e.response["error"]
+		)
 
 
-    send_slack_message(message)
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
-    
-# Slack Message End
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0")

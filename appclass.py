36598156c03cc034654c69fac5200cde5d8fcbf2
konnorscott22 <konnorscott@tamu.edu>
2021-11-09 21:18:37 -0600

    Update appclass.py

--- appclass.py
+++ appclass.py
@@ -118,7 +118,7 @@ status_code = " "
 app = Flask(__name__)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', port=5000)
 
 @app.route('/keyval', methods = ['POST'])
 def post():
